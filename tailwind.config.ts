6ca113f910348241cbe3ad5578a12f8d2b776840
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-12 16:31:32 +0500

    Explore skill

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customColor: {
+          primary: "#24f8c7", // Define your custom color here
+          // Add more custom color variations as needed
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        primary:
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", // Define boxShadow utility class
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
